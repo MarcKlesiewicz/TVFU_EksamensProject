685682cf810c3e53342c75f98259920a697e45c2
Anders <asbn26877@edu.ucl.dk>
2021-04-21 10:48:15 +0200

    Added Unit Test projektet

---
+++ TVFU_System/UnitTests/UC01-Controller_Tests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using DomainLayer.EventArgs;
+using Application.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Persistence.Repositories.Implementations;
+using Persistence.Data;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UC01_Controller_Tests
+    {
+        ProductListController controller;
+        ProductRepo repo;
+        TextFileWriter dataWriter;
+
+        [TestInitialize]
+        public void Init()
+        {
+            dataWriter = new TextFileWriter();
+            repo = new ProductRepo(dataWriter);
+        }
+
+        [TestMethod]
+        public void CanCreateProductTestWithNull()
+        {
+            //Arrange
+            controller = new ProductListController(repo);
+
+            //Act
+            controller.NewProductRequested += NewProductRequestedHandlerNull;
+            controller.CreateProduct(null);
+
+            //Assert
+            using (StreamReader reader = new StreamReader(@"Data\TextFile.ini"))
+            {
+                Assert.AreEqual(true, reader.EndOfStream);
+            }
+        }
+
+        [TestMethod]
+        public void CanCreateProductTestWithoutNull()
+        {
+            //Arrange
+            controller = new ProductListController(repo);
+
+            //Act
+            controller.NewProductRequested += NewProductRequestedHandlerNotNull;
+            controller.CreateProduct(null);
+
+            //Assert
+            using (StreamReader reader = new StreamReader(@"Data\TextFile.ini"))
+            {
+                Assert.AreEqual("Mette Frederiksen", reader.ReadLine());
+            }
+        }
+
+        private ProductEventArgs NewProductRequestedHandlerNull(object sender, ProductEventArgs args)
+        {
+            ProductEventArgs result = null;
+            return result;
+        }
+
+        private ProductEventArgs NewProductRequestedHandlerNotNull(object sender, ProductEventArgs args)
+        {
+            ProductEventArgs result = new ProductEventArgs() { Name = "Mette Frederiksen" };
+            return result;
+        }
+    }
+}
