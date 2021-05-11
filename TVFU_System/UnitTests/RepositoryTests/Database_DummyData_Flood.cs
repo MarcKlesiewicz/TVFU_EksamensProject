7c2c9cea40c1dc528a4828b36f51dd80e3632dd2
Anders <asbn26877@edu.ucl.dk>
2021-05-11 13:33:23 +0200

    Lavet ny data flood metode til at fylde databasen op

--- TVFU_System/UnitTests/RepositoryTests/Database_DummyData_Flood.cs
+++ TVFU_System/UnitTests/RepositoryTests/Database_DummyData_Flood.cs
@@ -27,133 +27,276 @@ namespace UnitTests.RepositoryTests
     //        repo = new ProductRepo();
     //        random = new Random();
     //        testList = new List<ProductEventArgs>();
-    //        for (int i = 0; i < 1000; i++)
+    //        //for (int i = 0; i < 1000; i++)
+    //        //{
+    //        //    if (i % 3 == 0)
+    //        //    {
+    //        //        testList.Add(new ProductEventArgs()
+    //        //        {
+    //        //            Id = Guid.NewGuid().ToString(),
+    //        //            Description = $"Papkasse {100 + i} mm",
+    //        //            UnitPrice = (float)random.Next(1, 10000),
+    //        //            GuidingPrice = (float)random.Next(1, 10000),
+    //        //            TotalStock = random.Next(0, 100000),
+    //        //            Blocked = (random.Next(0, 15) == 0) ? true : false,
+    //        //            UnitPerPackage = random.Next(1, 1000),
+    //        //            QuantityDiscount = random.Next(0, 1000),
+    //        //            ConfirmedDeliveryDate = DateTime.Today,
+    //        //            ProductNumber = 100 + i,
+    //        //            CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
+    //        //            PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
+    //        //            ProductCategory = "Kasse"
+    //        //        });
+    //        //    }
+    //        //    else if (i % 5 == 0)
+    //        //    {
+    //        //        testList.Add(new ProductEventArgs()
+    //        //        {
+    //        //            Id = Guid.NewGuid().ToString(),
+    //        //            Description = $"Skuffe {100 + i} mm",
+    //        //            UnitPrice = (float)random.Next(1, 10000),
+    //        //            GuidingPrice = (float)random.Next(1, 10000),
+    //        //            TotalStock = random.Next(0, 100000),
+    //        //            Blocked = (random.Next(0, 15) == 0) ? true : false,
+    //        //            UnitPerPackage = random.Next(1, 1000),
+    //        //            QuantityDiscount = random.Next(0, 1000),
+    //        //            ConfirmedDeliveryDate = DateTime.Today,
+    //        //            ProductNumber = 100 + i,
+    //        //            CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
+    //        //            PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
+    //        //            ProductCategory = "Skuffe"
+    //        //        });
+    //        //    }
+    //        //    else if (i % 7 == 0)
+    //        //    {
+    //        //        testList.Add(new ProductEventArgs()
+    //        //        {
+    //        //            Id = Guid.NewGuid().ToString(),
+    //        //            Description = $"Kubik montage {100 + i} mm",
+    //        //            UnitPrice = (float)random.Next(1, 10000),
+    //        //            GuidingPrice = (float)random.Next(1, 10000),
+    //        //            TotalStock = random.Next(0, 100000),
+    //        //            Blocked = (random.Next(0, 15) == 0) ? true : false,
+    //        //            UnitPerPackage = random.Next(1, 1000),
+    //        //            QuantityDiscount = random.Next(0, 1000),
+    //        //            ConfirmedDeliveryDate = DateTime.Today,
+    //        //            ProductNumber = 100 + i,
+    //        //            CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
+    //        //            PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
+    //        //            ProductCategory = "Kubik"
+    //        //        });
+    //        //    }
+    //        //    else if (i % 11 == 0)
+    //        //    {
+    //        //        testList.Add(new ProductEventArgs()
+    //        //        {
+    //        //            Id = Guid.NewGuid().ToString(),
+    //        //            Description = $"Reol gøgl {100 + i} mm",
+    //        //            UnitPrice = (float)random.Next(1, 10000),
+    //        //            GuidingPrice = (float)random.Next(1, 10000),
+    //        //            TotalStock = random.Next(0, 100000),
+    //        //            Blocked = (random.Next(0, 15) == 0) ? true : false,
+    //        //            UnitPerPackage = random.Next(1, 1000),
+    //        //            QuantityDiscount = random.Next(0, 1000),
+    //        //            ConfirmedDeliveryDate = DateTime.Today,
+    //        //            ProductNumber = 100 + i,
+    //        //            CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
+    //        //            PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
+    //        //            ProductCategory = "Reol"
+    //        //        });
+    //        //    }
+    //        //    else if (i % 13 == 0)
+    //        //    {
+    //        //        testList.Add(new ProductEventArgs()
+    //        //        {
+    //        //            Id = Guid.NewGuid().ToString(),
+    //        //            Description = $"Hylde {100 + i} mm",
+    //        //            UnitPrice = (float)random.Next(1, 10000),
+    //        //            GuidingPrice = (float)random.Next(1, 10000),
+    //        //            TotalStock = random.Next(0, 100000),
+    //        //            Blocked = (random.Next(0, 15) == 0) ? true : false,
+    //        //            UnitPerPackage = random.Next(1, 1000),
+    //        //            QuantityDiscount = random.Next(0, 1000),
+    //        //            ConfirmedDeliveryDate = DateTime.Today,
+    //        //            ProductNumber = 100 + i,
+    //        //            CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
+    //        //            PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
+    //        //            ProductCategory = "Hylde"
+    //        //        });
+    //        //    }
+    //        //    else if (i % 17 == 0)
+    //        //    {
+    //        //        testList.Add(new ProductEventArgs()
+    //        //        {
+    //        //            Id = Guid.NewGuid().ToString(),
+    //        //            Description = $"Sokkel {100 + i} mm",
+    //        //            UnitPrice = (float)random.Next(1, 10000),
+    //        //            GuidingPrice = (float)random.Next(1, 10000),
+    //        //            TotalStock = random.Next(0, 100000),
+    //        //            Blocked = (random.Next(0, 15) == 0) ? true : false,
+    //        //            UnitPerPackage = random.Next(1, 1000),
+    //        //            QuantityDiscount = random.Next(0, 1000),
+    //        //            ConfirmedDeliveryDate = DateTime.Today,
+    //        //            ProductNumber = 100 + i,
+    //        //            CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
+    //        //            PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
+    //        //            ProductCategory = "Sokkel"
+    //        //        });
+    //        //    }
+    //        //    else
+    //        //    {
+    //        //        testList.Add(new ProductEventArgs()
+    //        //        {
+    //        //            Id = Guid.NewGuid().ToString(),
+    //        //            Description = $"Dubloklods (træ) {100 + i} mm",
+    //        //            UnitPrice = (float)random.Next(1, 10000),
+    //        //            GuidingPrice = (float)random.Next(1, 10000),
+    //        //            TotalStock = random.Next(0, 100000),
+    //        //            Blocked = (random.Next(0, 15) == 0) ? true : false,
+    //        //            UnitPerPackage = random.Next(1, 1000),
+    //        //            QuantityDiscount = random.Next(0, 1000),
+    //        //            ConfirmedDeliveryDate = DateTime.Today,
+    //        //            ProductNumber = 100 + i,
+    //        //            CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
+    //        //            PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
+    //        //            ProductCategory = "Dublo"
+    //        //        });
+    //        //    }
+    //        //}
+    //        for (int i = 0; i < 4000; i++)
     //        {
+    //            string dateTime = $"{random.Next(10, 31)}/{random.Next(10, 12)}/{random.Next(2000, 2021)}";
     //            if (i % 3 == 0)
     //            {
     //                testList.Add(new ProductEventArgs()
     //                {
     //                    Id = Guid.NewGuid().ToString(),
-    //                    Description = $"Papkasse {100 + i} mm",
+    //                    Description = (random.Next(0, 1) == 0) ? $"ABC Grundreol Birk Grøn {100 + i} mm" : $"ABC Tilbygningsreol Kirsebær Blå {100 + i} mm",
     //                    UnitPrice = (float)random.Next(1, 10000),
     //                    GuidingPrice = (float)random.Next(1, 10000),
     //                    TotalStock = random.Next(0, 100000),
     //                    Blocked = (random.Next(0, 15) == 0) ? true : false,
     //                    UnitPerPackage = random.Next(1, 1000),
     //                    QuantityDiscount = random.Next(0, 1000),
-    //                    ConfirmedDeliveryDate = DateTime.Today,
+    //                    ConfirmedDeliveryDate = DateTime.Parse(dateTime),
     //                    ProductNumber = 100 + i,
     //                    CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
     //                    PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
-    //                    ProductCategory = "Kasse"
+    //                    ProductCategory = "ABC",
     //                });
-    //            } else if (i % 5 == 0)
+    //            }
+    //            else if (i % 5 == 0)
     //            {
     //                testList.Add(new ProductEventArgs()
     //                {
     //                    Id = Guid.NewGuid().ToString(),
-    //                    Description = $"Skuffe {100 + i} mm",
+    //                    Description = (random.Next(0, 1) == 0) ? $"KUBIK Color hel reolkasse Akacie Sort {100 + i} mm" : $"KUBIK halv reolkasse Fyr Grøn {100 + i} mm",
     //                    UnitPrice = (float)random.Next(1, 10000),
     //                    GuidingPrice = (float)random.Next(1, 10000),
     //                    TotalStock = random.Next(0, 100000),
     //                    Blocked = (random.Next(0, 15) == 0) ? true : false,
     //                    UnitPerPackage = random.Next(1, 1000),
     //                    QuantityDiscount = random.Next(0, 1000),
-    //                    ConfirmedDeliveryDate = DateTime.Today,
+    //                    ConfirmedDeliveryDate = DateTime.Parse(dateTime),
     //                    ProductNumber = 100 + i,
     //                    CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
     //                    PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
-    //                    ProductCategory = "Skuffe"
+    //                    ProductCategory = "KUBIK"
     //                });
-    //            } else if (i % 7 == 0)
+    //            }
+    //            else if (i % 7 == 0)
     //            {
     //                testList.Add(new ProductEventArgs()
     //                {
     //                    Id = Guid.NewGuid().ToString(),
-    //                    Description = $"Kubik montage {100 + i} mm",
+    //                    Description = (random.Next(0, 1) == 0) ? $"Lænestol i formspændt Bøg Rød {100 + i} mm" : $"Fodskammel i formspændt i formspændt Eg Gul {100 + i} mm",
     //                    UnitPrice = (float)random.Next(1, 10000),
     //                    GuidingPrice = (float)random.Next(1, 10000),
     //                    TotalStock = random.Next(0, 100000),
     //                    Blocked = (random.Next(0, 15) == 0) ? true : false,
     //                    UnitPerPackage = random.Next(1, 1000),
     //                    QuantityDiscount = random.Next(0, 1000),
-    //                    ConfirmedDeliveryDate = DateTime.Today,
+    //                    ConfirmedDeliveryDate = DateTime.Parse(dateTime),
     //                    ProductNumber = 100 + i,
     //                    CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
     //                    PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
-    //                    ProductCategory = "Kubik"
+    //                    ProductCategory = "Møbler"
     //                });
-    //            } else if (i % 11 == 0)
+    //            }
+    //            else if (i % 11 == 0)
     //            {
     //                testList.Add(new ProductEventArgs()
     //                {
     //                    Id = Guid.NewGuid().ToString(),
-    //                    Description = $"Reol gøgl {100 + i} mm",
+    //                    Description = (random.Next(0, 1) == 0) ? $"Sticks bordplade Birk Rød {100 + i}x{90 + i} mm" : $"Sticks bordplade Akacie Hvid {100 + i}x{90 + i} mm",
     //                    UnitPrice = (float)random.Next(1, 10000),
     //                    GuidingPrice = (float)random.Next(1, 10000),
     //                    TotalStock = random.Next(0, 100000),
     //                    Blocked = (random.Next(0, 15) == 0) ? true : false,
     //                    UnitPerPackage = random.Next(1, 1000),
     //                    QuantityDiscount = random.Next(0, 1000),
-    //                    ConfirmedDeliveryDate = DateTime.Today,
+    //                    ConfirmedDeliveryDate = DateTime.Parse(dateTime),
     //                    ProductNumber = 100 + i,
     //                    CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
     //                    PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
-    //                    ProductCategory = "Reol"
+    //                    ProductCategory = "Køkken"
     //                });
-    //            } else if (i % 13 == 0)
+    //            }
+    //            else if (i % 13 == 0)
     //            {
     //                testList.Add(new ProductEventArgs()
     //                {
     //                    Id = Guid.NewGuid().ToString(),
-    //                    Description = $"Hylde {100 + i} mm",
+    //                    Description = (random.Next(0, 1) == 0) ? $"Musvitkasse Kirsebær Gul {100 + i} mm hul" : $"Mejsekasse Fyr Hvid {100 + i} mm hul",
     //                    UnitPrice = (float)random.Next(1, 10000),
     //                    GuidingPrice = (float)random.Next(1, 10000),
     //                    TotalStock = random.Next(0, 100000),
     //                    Blocked = (random.Next(0, 15) == 0) ? true : false,
     //                    UnitPerPackage = random.Next(1, 1000),
     //                    QuantityDiscount = random.Next(0, 1000),
-    //                    ConfirmedDeliveryDate = DateTime.Today,
+    //                    ConfirmedDeliveryDate = DateTime.Parse(dateTime),
     //                    ProductNumber = 100 + i,
     //                    CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
     //                    PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
-    //                    ProductCategory = "Hylde"
+    //                    ProductCategory = "Have"
     //                });
-    //            } else if (i % 17 == 0)
+    //            }
+    //            else if (i % 17 == 0)
     //            {
     //                testList.Add(new ProductEventArgs()
     //                {
     //                    Id = Guid.NewGuid().ToString(),
-    //                    Description = $"Sokkel {100 + i} mm",
+    //                    Description = (random.Next(0, 1) == 0) ? $"Le Corbusier stol Bøg Blå {100 + i} mm" : $"Le Corbusier stol Eg Gul {100 + i} mm",
     //                    UnitPrice = (float)random.Next(1, 10000),
     //                    GuidingPrice = (float)random.Next(1, 10000),
     //                    TotalStock = random.Next(0, 100000),
     //                    Blocked = (random.Next(0, 15) == 0) ? true : false,
     //                    UnitPerPackage = random.Next(1, 1000),
     //                    QuantityDiscount = random.Next(0, 1000),
-    //                    ConfirmedDeliveryDate = DateTime.Today,
+    //                    ConfirmedDeliveryDate = DateTime.Parse(dateTime),
     //                    ProductNumber = 100 + i,
     //                    CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
     //                    PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
-    //                    ProductCategory = "Sokkel"
+    //                    ProductCategory = "Møbler"
     //                });
-    //            } else
+    //            }
+    //            else
     //            {
     //                testList.Add(new ProductEventArgs()
     //                {
     //                    Id = Guid.NewGuid().ToString(),
-    //                    Description = $"Dubloklods (træ) {100 + i} mm",
+    //                    Description = (random.Next(0, 1) == 0) ? $"Vitrineskab i Kirsebær Hvid {100 + i} mm" : $"Vitrineskab i Birk Sort {100 + i} mm",
     //                    UnitPrice = (float)random.Next(1, 10000),
     //                    GuidingPrice = (float)random.Next(1, 10000),
     //                    TotalStock = random.Next(0, 100000),
     //                    Blocked = (random.Next(0, 15) == 0) ? true : false,
     //                    UnitPerPackage = random.Next(1, 1000),
     //                    QuantityDiscount = random.Next(0, 1000),
-    //                    ConfirmedDeliveryDate = DateTime.Today,
+    //                    ConfirmedDeliveryDate = DateTime.Parse(dateTime),
     //                    ProductNumber = 100 + i,
     //                    CountryOfOrigin = (random.Next(0, 8) == 0) ? "Tyskland" : "Danmark",
     //                    PurchasingManager = (random.Next(0, 1) == 0) ? "Henrik Lange" : "Anders Lange",
-    //                    ProductCategory = "Dublo"
+    //                    ProductCategory = "Møbler"
     //                });
     //            }
     //        }
